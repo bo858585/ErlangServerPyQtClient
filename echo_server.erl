6e1324f00cb60e7ac144369c7ac5b85be2034893
bo858585 <bo858585@gmail.com>
2012-06-15 15:23:08 +0400

    Update master

--- echo_server.erl
+++ echo_server.erl
@@ -1,16 +1,26 @@
 -module(echo_server).
 
--export([start/0, loop/1]).
+-export([start/1, loop/1]).
 
-% Ñòàðò ýõî-ñåðâåðà
-start() ->
-    socket_server:start(?MODULE, 7001, {?MODULE, loop}).
+% Ð¡Ñ‚Ð°Ñ€Ñ‚ ÑÑ…Ð¾-ÑÐµÑ€Ð²ÐµÑ€Ð°
+start(Port) ->
+    socket_server:start(?MODULE, Port, {?MODULE, loop}).
 
 loop(Socket) ->
     case gen_tcp:recv(Socket, 0) of
-        {ok, Data} ->
-            gen_tcp:send(Socket, Data),
-            loop(Socket);
+        {ok, Data} ->            
+            io:format("Data ~p~n",[Data]),
+            if 
+                Data == <<"stop">> ->
+                    io:format("Exit ~p~n",[Data]),
+                    gen_tcp:close(Socket),
+                    exit("Client said stop."),  
+                    io:format("You don't  ~p~n",[Data]);
+                true ->
+                gen_tcp:send(Socket, Data),
+	            io:format("Sended ~p~n",[Data]),
+	            loop(Socket) 
+            end;
         {error, closed} ->
             ok
     end.
